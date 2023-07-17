3f5ef826efc67961c9ccb94761227fa7777eebb7
dmitrymelnichenko <qube.mel99@gmail.com>
2023-07-17 16:51:46 +0700

    endpoints_creating

---
+++ src/test/java/autotests/clients/DuckClient.java
@@ -0,0 +1,64 @@
+package autotests.clients;
+
+import autotests.EndpointConfig;
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.http.client.HttpClient;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Description;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+@ContextConfiguration(classes = {EndpointConfig.class})
+public class DuckClient extends TestNGCitrusSpringSupport {
+
+    @Autowired
+    protected HttpClient duckService;
+
+    @Description("CreateEndpoint")
+    public void duckCreate(TestCaseRunner runner, String color, String height, String material, String sound, String wingsState) {
+        runner.$(http().client(duckService)
+                .send()
+                .post("/api/duck/create")
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body("{\n" +
+                        "  \"color\": \"" + color + "\",\n" +
+                        "  \"height\": " + height + ",\n" +
+                        "  \"material\": \"" + material + "\",\n" +
+                        "  \"sound\": \"" + sound + "\",\n" +
+                        "  \"wingsState\": \"" + wingsState + "\"\n" +
+                        "}"));
+    }
+
+    @Description("DeleteEndpoint")
+    public void duckDelete(TestCaseRunner runner, String id) {
+        runner.$(http().client(duckService)
+                .send()
+                .delete("/api/duck/delete")
+                .queryParam("id", id));
+    }
+
+    @Description("GetAllIdsEndpoint")
+    public void duckGetAllIds(TestCaseRunner runner) {
+        runner.$(http().client(duckService)
+                .send()
+                .get("/api/duck/getAllIds"));
+    }
+
+    @Description("UpdateEndpoint")
+    public void duckUpdate(TestCaseRunner runner, String color, String height, String id, String material, String sound, String wingsState) {
+        runner.$(http().client(duckService)
+                .send()
+                .put("/api/duck/update")
+                .queryParam("color", color)
+                .queryParam("height", height)
+                .queryParam("id", id)
+                .queryParam("material", material)
+                .queryParam("sound", sound)
+                .queryParam("wingsState", wingsState));
+    }
+}
