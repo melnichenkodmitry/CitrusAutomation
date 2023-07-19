e07daf746cbd18cbdf4221848f0b6b42e8d227a2
dmitrymelnichenko <qube.mel99@gmail.com>
2023-07-19 19:35:03 +0700

    add payloads and validations

---
+++ src/test/java/autotests/payloads/Duck.java
@@ -0,0 +1,31 @@
+package autotests.payloads;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * Ответ create
+ */
+
+@Setter //установка значения свойств
+@Getter //установка значения свойств
+@Accessors(fluent = true) //создание методов доступа (геттеров и сеттеров)
+@JsonInclude(JsonInclude.Include.NON_NULL) //игнорирование свойств объекта равных null
+public class Duck {
+    @JsonProperty
+    private Integer id;
+    @JsonProperty
+    private String color;
+    @JsonProperty
+    private Integer height;
+    @JsonProperty
+    private String material;
+    @JsonProperty
+    private String sound;
+    @JsonProperty
+    private String wingsState;
+
+}
